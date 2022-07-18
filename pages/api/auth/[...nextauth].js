86f504db9038245079d68be69213c5641c27ad66
Son Dao <email@example.com>
2022-07-18 13:39:37 -0700

    added mongo-db functionality

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import TwitterProvider from "next-auth/providers/twitter";
+import GoogleProvider from "next-auth/providers/google";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "../../../lib/mongodb";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -13,9 +16,16 @@ export default NextAuth({
       clientId: process.env.TWITTER_CLIENT_ID,
       clientSecret: process.env.TWITTER_CLIENT_SECRET,
     }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+
     // ...add more providers here
   ],
+  adapter: MongoDBAdapter(clientPromise),
   pages: {
     signIn: "/auth/signin",
   },
+  // adapter: MongoDBAdapter(clientPromise),
 });
